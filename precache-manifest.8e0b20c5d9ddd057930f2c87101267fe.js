3d8d2884e083e642be268b5580389ced1ebe9b28
Vicario Williams <vawilliams@github.com>
2020-07-31 00:23:50 +0200

    Updates

---
+++ precache-manifest.8e0b20c5d9ddd057930f2c87101267fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdc4cc735a57ecccb0a7233f8a28933",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "42672945049c8ffd10e0",
+    "url": "/Drum-Machine/static/css/2.35f4b9e2.chunk.css"
+  },
+  {
+    "revision": "d6faf17c60ba9be638ea",
+    "url": "/Drum-Machine/static/css/main.0c3c4ba1.chunk.css"
+  },
+  {
+    "revision": "42672945049c8ffd10e0",
+    "url": "/Drum-Machine/static/js/2.2647167d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drum-Machine/static/js/2.2647167d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6faf17c60ba9be638ea",
+    "url": "/Drum-Machine/static/js/main.069683b3.chunk.js"
+  },
+  {
+    "revision": "c6dd1430e2afb8c8d750",
+    "url": "/Drum-Machine/static/js/runtime-main.d322cb89.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Drum-Machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Drum-Machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Drum-Machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Drum-Machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Drum-Machine/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
